1b544b0a57a7f1124929547c39ff7eaa1bad52d0
kaede <biopaint1024@gmail.com>
2022-02-06 00:00:10 +0900

    エクセルファイルのB列に打たれた英文の和訳をC列に出力

--- python/web_scryping/Auto_browser_test/deepl.py
+++ python/web_scryping/Auto_browser_test/deepl.py
@@ -1,19 +1,30 @@
 #使用ライブラリ
+from tracemalloc import start
 import deepl_get
 import openpyxl
+import time
+
+start_time = time.process_time()
+#CPU処理時間計測開始
 
 book = openpyxl.load_workbook('English.xlsx')
 #openpyxlでエクセルファイルの読み込み
 sheet = book['Sheet1']
 #エクセルのシートの指定
-strings = sheet.cell(row=2,column=2).value
-#読み込みたいシートのセルを指定
-output = deepl_get.deepl(strings)
-sheet.cell(row = 2, column = 3).value = output
-#特定のセルへの書き込み
-book.save('English.xlsx')
 
+for i in range(2,7):
+    strings = sheet.cell(row = i, column = 2).value
+#読み込みたいシートのセルを指定
+    output = deepl_get.deepl(strings)
+#自作関数(deep_getファイル参照)
+    sheet.cell(row = i, column = 3).value = output
+#特定のセルへの書き込み
+
+book.save('English.xlsx')
 #シートの保存　これを行わないと反映されません
+
+end_time = time.process_time()
+print("処理時間", end_time - start_time)
 
 
 
