77cd2154374635eb5a7a61bc13fb5235d5bc0da5
kaede <biopaint1024@gmail.com>
2022-02-06 17:00:58 +0900

    エクセルファイルの英文が入力された行の指定を不要にした

--- python/web_scryping/Auto_browser_test/deepl.py
+++ python/web_scryping/Auto_browser_test/deepl.py
@@ -1,18 +1,25 @@
 #使用ライブラリ
 from tracemalloc import start
 import deepl_get
+import deepl_txt
 import openpyxl
 import time
 
+
 start_time = time.process_time()
 #CPU処理時間計測開始
+
+deepl_txt.input_txt()
 
 book = openpyxl.load_workbook('English.xlsx')
 #openpyxlでエクセルファイルの読み込み
 sheet = book['Sheet1']
 #エクセルのシートの指定
 
-for i in range(2,7):
+for i in range(2,100):
+    if(sheet.cell(row = i, column = 2).value == None):
+        break
+#読み込むインデックスの範囲を数字を入れて指定する。
     strings = sheet.cell(row = i, column = 2).value
 #読み込みたいシートのセルを指定
     output = deepl_get.deepl(strings)
@@ -22,6 +29,8 @@ for i in range(2,7):
 
 book.save('English.xlsx')
 #シートの保存　これを行わないと反映されません
+
+deepl_get.close()
 
 end_time = time.process_time()
 print("処理時間", end_time - start_time)
