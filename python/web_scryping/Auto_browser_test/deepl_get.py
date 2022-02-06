405844d3c412878afbe22045901019bdd8bb654f
kaede <biopaint1024@gmail.com>
2022-02-07 00:27:15 +0900

    deepLのサイトに接続 var1.0

--- python/web_scryping/Auto_browser_test/deepl_get.py
+++ python/web_scryping/Auto_browser_test/deepl_get.py
@@ -2,20 +2,28 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 
-browser = Service(executable_path="C:\\Users\\FMV\\Desktop\\program\\python\\web_scryping\\chromedriver.exe")
+browser = Service(executable_path="chromedriver.exe")
 #使用ドライバの選択 (ブラウザのバージョンに留意)
+url = "https://www.deepl.com/ja/translator"
+
+options = Options()
+options.add_argument('--headless')
+#ブラウザの非表示
+
+#importした際にここの処理を実行されないようにする
 driver = webdriver.Chrome(service = browser)
+#ブラウザの立ち上げ
 #Serviceオブジェクトにpathを渡してから、webdriverに渡す
 driver.implicitly_wait(3)
-url = "https://www.deepl.com/ja/translator"
 driver.get(url)
 time.sleep(5)
 #相手のサーバーに負荷をかけないためにも必須な処理
 input = driver.find_element(By.XPATH,'/html/body/div[3]/main/div[3]/div[3]/section[1]/div[2]/div[2]/textarea')
+#XPATHで取得したい情報の場所の指定
 print("アクセス成功")
-
 
 def deepl(string):
 #html(検証)から取得する関数
